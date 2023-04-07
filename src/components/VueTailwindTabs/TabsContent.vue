4e62a8b2ce565b4489a4861effd6558578472f6c
Selemondev <selemondev@localhost.localdomain>
2023-04-07 15:41:18 -0500

    feat: add TabsContent

--- src/components/VueTailwindTabs/TabsContent.vue
+++ src/components/VueTailwindTabs/TabsContent.vue
@@ -1,9 +1,17 @@
 <script setup lang='ts'>
+export interface Props {
+    textColor: string
+};
 
+const props = withDefaults(defineProps<Props>(), {
+    textColor: 'text-black'
+})
 </script>
 
 <template>
-    <div>
-
+    <div class="bg-white rounded-md">
+        <div :class="props.textColor">
+            <slot/>
+        </div>
     </div>
 </template>
