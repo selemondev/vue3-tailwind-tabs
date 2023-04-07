535bc7cac6603c0d3154adeb415ac4fe38e8fee2
Selemondev <selemondev@localhost.localdomain>
2023-04-07 15:27:22 -0500

    feat: add Tab

--- src/components/VueTailwindTabs/Tab.vue
+++ src/components/VueTailwindTabs/Tab.vue
@@ -1,17 +1,20 @@
 <script setup lang='ts'>
-defineProps<{
-    title: String,
-    isActive: String,
-    activeTitleColor: String,
-    onClick: () => void
-}>()
+export interface Props {
+    title: string,
+    isActive: boolean,
+    activeTitleColor: string,
+    onClick: () => void,
+}
+const props = withDefaults(defineProps<Props>(), {
+    activeTitleColor: 'text-purple-500'
+});
 </script>
 
 <template>
     <div>
-        <li @click="onClick" :class="[isActive ? 'flex-center space-x-2' : '']">
+        <li @click="onClick" :class="[isActive ? 'flex-center border-b-2 border-purple-600 active' : 'border-b-2 border-transparent']">
             <slot name="icon"></slot>
-            <span :class="{ isActive : activeTitleColor }">{{ title }}</span>
+            <span :class="{ isActive : props.activeTitleColor }">{{ title }}</span>
         </li>
     </div>
 </template>
