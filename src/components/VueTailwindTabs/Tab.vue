089b9088f12beb3877a4f6e42cf037323b134ce4
Selemondev <selemondev@localhost.localdomain>
2023-04-07 15:11:40 -0500

    initial commit

---
+++ src/components/VueTailwindTabs/Tab.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+defineProps<{
+    title: String,
+    isActive: String,
+    activeTitleColor: String,
+    onClick: () => void
+}>()
+</script>
+
+<template>
+    <div>
+        <li @click="onClick" :class="[isActive ? 'flex-center space-x-2' : '']">
+            <slot name="icon"></slot>
+            <span :class="{ isActive : activeTitleColor }">{{ title }}</span>
+        </li>
+    </div>
+</template>
