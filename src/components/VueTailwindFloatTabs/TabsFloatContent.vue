0afe413b449aa02da7e30b18be4ae6bafcaacd84
selemondev <selemondev19@gmail.com>
2023-04-09 03:06:54 -0500

    chore: remove unused dependencies

--- src/components/VueTailwindFloatTabs/TabsFloatContent.vue
+++ src/components/VueTailwindFloatTabs/TabsFloatContent.vue
@@ -1,16 +1,19 @@
 <script setup lang='ts'>
 export interface Props {
     TabsFloatContentTextColor?: string
-    TabsFloatContentWidth?: number | string
+    TabsFloatContentWidth?: number | string,
+    TabsFloatContentBackgroundColor?: string
 };
 
 const props = withDefaults(defineProps<Props>(), {
     TabsFloatContentTextColor: 'text-black dark:text-white',
+    TabsFloatContentBackgroundColor: 'bg-gray-100 dark:bg-gray-400/30'
+
 });
 </script>
 
 <template>
-    <section :class="[ props.TabsFloatContentWidth ? `rounded-md p-4 w-72 sm:w-${props.TabsFloatContentWidth}` : `w-72 p-4 rounded-md bg-gray-100 dark:bg-gray-500 sm:w-[768px]`]" class="my-4">
+    <section :class="[props.TabsFloatContentWidth ? `rounded-md p-4 w-${props.TabsFloatContentWidth} ${props.TabsFloatContentBackgroundColor}` : `rounded-md p-4 w-72 ${props.TabsFloatContentBackgroundColor} sm:w-[768px]`]" class="my-4">
         <div :class="props.TabsFloatContentTextColor">
             <slot/>
         </div>
